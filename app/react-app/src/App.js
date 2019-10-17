fe36a6f814746a3b1895f16b18029735c04281d9
teddyauthors <55211921+teddyauthors@users.noreply.github.com>
2019-10-17 14:11:50 -0700

    fixing attribute propagation error

--- app/react-app/src/App.js
+++ app/react-app/src/App.js
@@ -7,27 +7,39 @@ import * as d3 from "d3";
 
 class App extends React.Component {
   constructor(props) {
-
-    super(props);
-
+    super(props);  
     this.state = {
+      schemaLoaded: false,
       attributes: [],
       reduction: 'pca',
       brushing: false,
       bizId: 'all',
       cid: -1
     };
-
+    this.loadSchema = this.loadSchema.bind(this);
     this.onSelectReductionMethod = this.onSelectReductionMethod.bind(this);
     this.onEnableBrush = this.onEnableBrush.bind(this);
     this.onResetVis = this.onResetVis.bind(this);
     this.onLoadHotelCluster = this.onLoadHotelCluster.bind(this);
-    var schema_address = process.env.REACT_APP_SERVER_ADDRESS + 'data/schema.json';
+  }
+
+  UNSAFE_componentWillMount(){
+    // console.log("loadSchema set state")
+    this.loadSchema(); 
+
+  }
+
+  loadSchema(){
+    let that = this; 
+    const schema_address = process.env.REACT_APP_SERVER_ADDRESS + 'data/schema.json';
     d3.json(schema_address).then(data => 
     {
-      this.setState({attributes: data.schema})
+      that.setState({attributes: data.schema, schemaLoaded: true});
+      // console.log('Setting state')
+      // console.log(data)
     });
   }
+  
 
   onSelectPoints(points) {
     console.log(points.count());
@@ -52,8 +64,8 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div className="wrap">
+    return  this.state.schemaLoaded ? 
+      (<div className="wrap">
         <EntityPane 
           onLoadHotelCluster={ this.onLoadHotelCluster }
           attributes={ this.state.attributes } />
@@ -67,8 +79,7 @@ class App extends React.Component {
           bizId={ this.state.bizId }
           hotelName={ this.state.hotelName }
           rcount={ this.state.rcount } />
-      </div>
-    );
+      </div>) : (<div className="wrap"></div>);
   }
 }
 
